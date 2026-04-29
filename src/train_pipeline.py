5728c5ac220c25bdea87ddc5bf40faba46babc23
Sonal Rana <sonalr468@gmail.com>
2026-04-28 17:27:53 -0700

    Added more models

--- src/train_pipeline.py
+++ src/train_pipeline.py
@@ -6,8 +6,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LogisticRegression
-
-
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 #Load + clean data
 #df = pd.read_csv("../data/raw/Telco-Customer-Churn.csv")
 df = pd.read_csv("data/raw/Telco-Customer-Churn.csv")
@@ -17,7 +17,7 @@ df = df.drop(['customerID'], axis=1)
 
 df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
 df = df[df['tenure'] != 0]
-df['TotalCharges'].fillna(df['TotalCharges'].mean(), inplace=True)
+df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].mean())
 
 df["SeniorCitizen"] = df["SeniorCitizen"].map({0: "No", 1: "Yes"})
 df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
@@ -32,9 +32,10 @@ X = df.drop('Churn', axis=1)
 y = df['Churn']
 
 
+
 #Define column types
-num_cols = ['tenure', 'MonthlyCharges', 'TotalCharges', 'AvgCharges']
-cat_cols = X.select_dtypes(include='object').columns.tolist()
+num_cols = ['tenure', 'MonthlyCharges', 'TotalCharges']
+cat_cols = X.select_dtypes(include=['object', 'string']).columns.tolist()
 
 #Build pipeline
 preprocessor = ColumnTransformer([
@@ -44,15 +45,67 @@ preprocessor = ColumnTransformer([
 
 pipeline = Pipeline([
     ('preprocessor', preprocessor),
-    ('model', LogisticRegression(max_iter=1000))
+    ('model', LogisticRegression(max_iter=1000,class_weight='balanced'))
 ])
 
 
-#Train pipeline
-pipeline.fit(X, y)
-
+# Split 
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+# train
+pipeline.fit(X_train, y_train)
+models = {"Logistic Regression": LogisticRegression(max_iter=1000, class_weight='balanced'),
+    "Random Forest": RandomForestClassifier(n_estimators=100, class_weight='balanced', random_state=42),
+    "Gradient Boosting": GradientBoostingClassifier(random_state=42) }
+#evaluate prediction
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+results = {}
+for name, model in models.items():
+    print(f"\n{'/'*40}")
+    print(f"Model: {name}")
+    print(f"{'/'*40}")
+    
+    # Create pipeline for each model
+    pipeline = Pipeline([
+        ('preprocessor', preprocessor),
+        ('model', model)
+    ])
+    
+    # Train
+    pipeline.fit(X_train, y_train)
+    
+    # Predict
+    y_pred = pipeline.predict(X_test)
+    
+    # Confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+    
+    # Store results
+    results[name] = cm
+    
+    # Print metrics
+    print("Confusion Matrix:\n", cm)
+    print("\nClassification Report:\n")
+    print(classification_report(y_test, y_pred))
+    
+    # Plot
+    plt.figure(figsize=(5,4))
+    sns.heatmap(cm, 
+                annot=True, 
+                fmt='d', 
+                cmap='Blues',
+                xticklabels=['No Churn', 'Churn'],
+                yticklabels=['No Churn', 'Churn'])
+    
+    plt.title(f"{name} - Confusion Matrix")
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.tight_layout()
+    plt.show()
 
 #Save pipeline
 joblib.dump(pipeline, 'models/churn_pipeline.pkl')
-
 print("Pipeline trained and saved successfully.")
